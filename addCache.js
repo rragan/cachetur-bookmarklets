2cacc0a3f504e380e7be1c84159cc536bac0d2f2
Richard Ragan <rragan@gmail.com>
2018-05-07 16:03:37 -0700

    Make small mobile devices work

--- addCache.js
+++ addCache.js
@@ -1,16 +1,16 @@
 javascript:function cacheturAdd() { 
     var tripId = localStorage.getItem("ctTrip");
     var gccode;
-    var mainDiv;
+    var mainDiv = document.getElementById("Content");
+
+    var where;
     if (location.href.indexOf("geocaching.com/map") != -1) {
     	var codeElts = document.getElementsByClassName("code");
-    	if (codeElts !== undefined) {
+    	if (codeElts.length > 0) {
     	    gccode = codeElts[0].innerHTML;
-    	    mainDiv = document.getElementsByClassName("wrapper")[0];
     	}
     }
     if (location.href.indexOf("geocaching.com/geocache") != -1) {
-         mainDiv = document.getElementById("divContentMain");
    	 var ctParts = location.href.split("/");
    	 gccode = ctParts[ctParts.length - 1].split("_");
     }
@@ -23,7 +23,7 @@ javascript:function cacheturAdd() {
             if (req.readyState === 4) {
                 if (req.status >= 200 && req.status < 400) {
                     var okMsg = '<div id="ctOkMsg" style="position:fixed;top:200px;left:200px;z-index:9999;font-size:20px;border:5px solid #0f0;opacity:1;background-color:#fff">&nbsp;&#x2714; ' +  localStorage.getItem("ctTripName") + '&nbsp;</div>';
-                    mainDiv.insertAdjacentHTML('afterbegin', okMsg);
+                    mainDiv.insertAdjacentHTML('beforebegin', okMsg);
                     setTimeout(function(){ var elem = document.querySelector('#ctOkMsg');
                     elem.parentNode.removeChild(elem); }, 2000);
                 }
@@ -33,4 +33,3 @@ javascript:function cacheturAdd() {
     }
 }
 cacheturAdd();
-
