b710a74c720312b2316fc5bdf45962a68dafc7a9
Richard Ragan <rragan@gmail.com>
2019-05-10 14:18:22 -0700

    Support for new geocaching map and Project GC maps

--- addCache.js
+++ addCache.js
@@ -1,34 +1,98 @@
-javascript:function cacheturAdd() { 
-    var tripId = localStorage.getItem("ctTrip");
-    var gccode;
-    var mainDiv = document.getElementById("Content");
+javascript:function cacheturAdd() {
+    var ctTripId = localStorage.getItem("ctTrip");
+    var ctGcCode;
+    var ctMsgHolder;
+    var ctCodeHolder;
+    var ctSplitParts;
+    if (_ctPage === "gc_map") {
+        ctCodeHolder = document.getElementsByClassName("code");
+        if (ctCodeHolder !== undefined && ctCodeHolder.length > 0) {
+            ctGcCode = ctCodeHolder[0].innerText;
+            ctMsgHolder = document.getElementsByClassName("map-wrapper")[0];
+        }
+    }
+    if (_ctPage === "gc_map_new") {
+        ctCodeHolder = document.getElementsByClassName("cache-metadata-code");
+        if (ctCodeHolder !== undefined && ctCodeHolder.length > 0) {
+            ctGcCode = ctCodeHolder[0].innerText;
+            ctMsgHolder = document.getElementsByClassName("preview-main")[0];
+        }
+    }
+    if (_ctPage === "gc_geocache") {
+        ctMsgHolder = document.getElementById("divContentMain");
+        ctSplitParts = location.href.split("/");
+        ctGcCode = ctSplitParts[ctSplitParts.length - 1].split("_");
+    }
 
-    if (location.href.indexOf("geocaching.com/map") != -1) {
-    	var codeElts = document.getElementsByClassName("code");
-    	if (codeElts.length > 0) {
-    	    gccode = codeElts[0].innerHTML;
-    	}
+    if (_ctPage === "pgc_map") {
+        ctCodeHolder = document.getElementsByClassName("leaflet-popup-content")[0];
+        if (ctCodeHolder !== undefined) {
+            ctCodeHolder = ctCodeHolder.firstChild;
+            ctSplitParts = ctCodeHolder.getAttribute("href").split("/");
+            ctGcCode = ctSplitParts[ctSplitParts.length - 1];
+            ctMsgHolder = document.getElementsByClassName("leaflet-container")[0];
+        }
     }
-    if (location.href.indexOf("geocaching.com/geocache") != -1) {
-   	 var ctParts = location.href.split("/");
-   	 gccode = ctParts[ctParts.length - 1].split("_");
-    }
-    
-    if (tripId && gccode) {
-        var req = new XMLHttpRequest();
-        req.open('GET', 'https://cachetur.no/api/planlagt_add_codes?format=json&tur=' + tripId + '&code='+gccode, true);
-        req.withCredentials = true;
-        req.onreadystatechange = function() {
-            if (req.readyState === 4) {
-                if (req.status >= 200 && req.status < 400) {
-                    var okMsg = '<div id="ctOkMsg" style="position:fixed;top:200px;left:200px;z-index:9999;font-size:20px;border:5px solid #0f0;opacity:1;background-color:#fff">&nbsp;&#x2714; ' +  localStorage.getItem("ctTripName") + '&nbsp;</div>';
-                    mainDiv.insertAdjacentHTML('beforebegin', okMsg);
-                    setTimeout(function(){ var elem = document.querySelector('#ctOkMsg');
-                    elem.parentNode.removeChild(elem); }, 2000);
+
+    if (ctTripId && ctGcCode) {
+        var ctReq = new XMLHttpRequest();
+        ctReq.open('GET', 'https://cachetur.no/api/planlagt_add_codes?format=json&tur=' + ctTripId + '&code=' + ctGcCode, true);
+        ctReq.withCredentials = true;
+        ctReq.onreadystatechange = function() {
+            if (ctReq.readyState === 4) {
+                if (ctReq.status >= 200 && ctReq.status < 400) {
+                    localStorage.setItem("lastGC", ctGcCode);
+                    var okMsg = '<div id="ctOkMsg" style="position:fixed;top:200px;left:200px;z-index:9999;font-size:20px;padding:10px;border:7px solid #0f0;opacity:1;background-color:#fff"><img src="https://cachetur.net/img/logo_top.png"/>' + localStorage.getItem("ctTripName") + '</div>';
+                    ctMsgHolder.insertAdjacentHTML('afterbegin', okMsg);
+                    setTimeout(function() {
+                        var elem = document.querySelector('#ctOkMsg');
+                        elem.parentNode.removeChild(elem);
+                    }, 2000);
                 }
             }
         };
-        req.send();
+        ctReq.send();
     }
 }
-cacheturAdd();
+function ctCheckLogin() {
+    var ctReq = new XMLHttpRequest();
+    ctReq.open("GET", "https://cachetur.no/api/user_get_current?format=json", true);
+    ctReq.withCredentials = true;
+    ctReq.onreadystatechange = function() {
+        if (4 === ctReq.readyState)
+            if (ctReq.status >= 200 && ctReq.status < 400) {
+                var ctData = JSON.parse(ctReq.responseText).data;
+                var _ctCacheturUser = ctData.username;
+                if (_ctCacheturUser === undefined || _ctCacheturUser === '') {
+                    alert("Please login to cachetur.no");
+                    return;
+                }
+                if (_ctPage !== "unknown") {
+                    cacheturAdd();
+                }
+            }
+    };
+    ctReq.send();
+}
+
+function ctGetPage() {
+var ctPath = window.location.pathname;
+var ctDomain = document.domain;
+
+if(ctDomain === "www.geocaching.com") {
+    if (ctPath.indexOf("/seek/") > -1) _ctPage = "gc_geocache";
+    else if (ctPath.indexOf("/geocache/") > -1) _ctPage = "gc_geocache";
+    else if (ctPath.indexOf("/play/map") > -1) _ctPage = "gc_map_new";
+    else if (ctPath.indexOf("/map") > -1) _ctPage = "gc_map";
+    
+}
+else if (ctDomain === "project-gc.com" && pathname.indexOf("/User/VirtualGPS") > -1 && window.location.search.indexOf("?map=") === -1) _ctPage = "pgc_vgps";
+else if (ctDomain === "project-gc.com") {
+    _ctPage = "pgc_map";
+}
+return _ctPage;
+}
+
+var _ctPage = "unknown";
+_ctPage = ctGetPage();
+ctCheckLogin();
